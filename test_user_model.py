9a49150d7832a2411c01480e1ccdac76504d6665
Colin Sidberry <colinsidberry@gmail.com>
2021-09-24 16:13:14 -0700

    added dependencies for heroku deployment

--- test_user_model.py
+++ test_user_model.py
@@ -4,17 +4,19 @@
 #
 #    python -m unittest test_user_model.py
 
+from sqlalchemy import exc
+from flask_wtf import form
+from models import db, User, Message, Follows
+from sqlalchemy.sql.schema import UniqueConstraint
+from unittest import TestCase
+from flask_bcrypt import Bcrypt
+from app import app
 import os
 
 os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
-from app import app
-from unittest import TestCase
-from sqlalchemy.sql.schema import UniqueConstraint
-from models import db, User, Message, Follows
-from flask_wtf import form
-from sqlalchemy import exc
 
+bcrypt = Bcrypt()
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -50,7 +52,9 @@ class UserModelTestCase(TestCase):
         u1 = User(
             email="test1@test.com",
             username="testuser1",
-            password="HASHED_PASSWORD"
+            # password="HASHED_PASSWORD"
+            password=bcrypt.generate_password_hash(
+                "HASHED_PASSWORD").decode('UTF-8')
         )
 
         u2 = User(
@@ -123,54 +127,74 @@ class UserModelTestCase(TestCase):
 
     def test_valid_user_classmethod_signup(self):
         """Tests for a valid user signup"""
-      
+
         num_users_before = len(User.query.all())
 
         User.signup(
-            username = 'testuser',
-            email = 'test@test.com',
-            password = 'password',
-            image_url = 'https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=1600&h=900'
+            username='testuser',
+            email='test@test.com',
+            password='password',
+            image_url='https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=1600&h=900'
         )
 
         self.assertEqual(len(User.query.all()), num_users_before + 1)
-    
+
     def test_invalid_user_classmethod_signup_dupe_username(self):
         """Tests for a valid user signup"""
 
         User.signup(
-            username = 'testuser1',
-            email = 'test@test.com',
-            password = 'password',
-            image_url = 'https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=1600&h=900'
+            username='testuser1',
+            email='test@test.com',
+            password='password',
+            image_url='https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=1600&h=900'
         )
 
         self.assertRaises(exc.IntegrityError)
-    
+
     def test_invalid_user_classmethod_signup_bad_email(self):
         """Tests for a valid user signup"""
 
         User.signup(
-            username = 'testuser',
-            email = 'test.com',
-            password = 'password',
-            image_url = 'https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=1600&h=900'
+            username='testuser',
+            email='test.com',
+            password='password',
+            image_url='https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=1600&h=900'
         )
 
         self.assertRaises(exc.IntegrityError)
-    
+
     def test_invalid_user_classmethod_signup_no_username_input(self):
         """Tests for a valid user signup"""
 
         User.signup(
-            username = None,
-            email = 'test.com',
-            password = 'password',
-            image_url = 'https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=1600&h=900'
+            username=None,
+            email='test.com',
+            password='password',
+            image_url='https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=1600&h=900'
         )
 
         self.assertRaises(exc.IntegrityError)
-    
-        # Does User.authenticate successfully return a user when given a valid username and password?
-        # Does User.authenticate fail to return a user when the username is invalid?
-        # Does User.authenticate fail to return a user when the password is invalid
+
+    def test_valid_user_authenticate_method(self):
+        """Tests the authenticate method for valid users"""
+
+        user = User.authenticate('testuser1', 'HASHED_PASSWORD')
+
+        self.assertEqual(self.u1, user)
+
+    # Does User.authenticate fail to return a user when the username is invalid?
+    def test_bad_username_authenticate_method(self):
+        """Tests the authenticate method fails for an invalid username."""    
+
+        User.authenticate('testuser', 'HASHED_PASSWORD')
+
+        self.assertRaises(exc.IntegrityError)
+
+    # Does User.authenticate fail to return a user when the password is invalid
+    def test_invalid_user_credentials_authenticate_method(self):
+        """Tests the authenticate method for invalid user credentials"""
+
+        user = User.authenticate('testuser1', 'BAD_PASSWORD')
+
+        self.assertEqual(user, False)
+
